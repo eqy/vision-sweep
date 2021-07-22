76517db103b1239281ca71cb4b2c2990f63a2ded
Eddie Yan <eddiey@nvidia.com>
2021-07-22 22:06:11 +0000

    train updates

--- prep.sh
+++ prep.sh
@@ -2,9 +2,29 @@
 gpu=""
 sm=""
 name=`nvidia-smi --query-gpu=name --format=csv --id=0`
-if [[ $(echo $name | grep 3080) ]]; then
+if [[ $(echo $name | grep '3080') ]]; then
 	echo "3080 Found"
 	gpu="3080"
+	sm="8.6"
+elif [[ $(echo $name | grep '3090') ]]; then
+	echo "3090 Found"
+	gpu="3090"
+	sm="8.6"
+elif [[ $(echo $name | grep 'A100') ]]; then
+	echo "A100 Found"
+	gpu="A100"
+	sm="8.0"
+elif [[ $(echo $name | grep 'V100') ]]; then
+	echo "V100 Found"
+	gpu="V100"
+	sm="7.0"
+elif [[ $(echo $name | grep 'A30') ]]; then
+	echo "A30 Found"
+	gpu="A30"
+	sm="8.0"
+elif [[ $(echo $name | grep 'A6000') ]]; then
+	echo "A6000 Found"
+	gpu="A6000"
 	sm="8.6"
 else
 	echo "No GPU"
@@ -17,14 +37,22 @@ if [[ $gpu ]]; then
 	../build.sh $sm --cmake
 	cd ../vision-sweep
 	pkill python
-	echo "running v8 heuristic mode b"
-	CUDNN_V8_API_ENABLED=1 CUDNN_V8_API_DEBUG=1 USE_HEURISTIC_MODE_B=1 python sweep.py --output v8heurb_"$gpu"_100.csv --sku $gpu
+	echo "running v8 heuristic mode b (torchvision)"
+	CUDNN_V8_API_ENABLED=1 CUDNN_V8_API_DEBUG=1 USE_HEURISTIC_MODE_B=1 python sweep.py --train --output train_v8heurb_"$gpu"_100.csv --sku $gpu
 	pkill python
-	echo "running v8 bench"
-	CUDNN_V8_API_ENABLED=1 CUDNN_V8_API_DEBUG=1 USE_HEURISTIC_MODE_B=0 python sweep.py --output v8bench_"$gpu"_100.csv --sku $gpu --bench
+	echo "running v8 bench (torchvision)"
+	CUDNN_V8_API_ENABLED=1 CUDNN_V8_API_DEBUG=1 USE_HEURISTIC_MODE_B=0 python sweep.py --train --output train_v8bench_"$gpu"_100.csv --sku $gpu --bench
 	pkill python
-	echo "running v7 bench"
-	CUDNN_V8_API_ENABLED=0 CUDNN_V8_API_DEBUG=1 USE_HEURISTIC_MODE_B=0 python sweep.py --output v7bench_"$gpu"_100.csv --sku $gpu --bench
+	echo "running v7 bench (torchvision)"
+	CUDNN_V8_API_ENABLED=0 CUDNN_V8_API_DEBUG=1 USE_HEURISTIC_MODE_B=0 python sweep.py --train --output train_v7bench_"$gpu"_100.csv --sku $gpu --bench
+	echo "running v8 heuristic mode b (timm)"
+	CUDNN_V8_API_ENABLED=1 CUDNN_V8_API_DEBUG=1 USE_HEURISTIC_MODE_B=1 python sweep.py --train --timm --output train_timm_v8heurb_"$gpu"_100.csv --sku $gpu
+	pkill python
+	echo "running v8 bench (timm)"
+	CUDNN_V8_API_ENABLED=1 CUDNN_V8_API_DEBUG=1 USE_HEURISTIC_MODE_B=0 python sweep.py --train --timm --output train_timm_v8bench_"$gpu"_100.csv --sku $gpu --bench
+	pkill python
+	echo "running v7 bench (timm)"
+	CUDNN_V8_API_ENABLED=0 CUDNN_V8_API_DEBUG=1 USE_HEURISTIC_MODE_B=0 python sweep.py --train --timm --output train_timm_v7bench_"$gpu"_100.csv --sku $gpu --bench
 
 else
 	echo "No gpu found, not running ..."
--- sweep.py
+++ sweep.py
@@ -26,6 +26,8 @@ def main():
     parser.add_argument('--native', action='store_true')
     parser.add_argument('--sku', type=str)
     parser.add_argument('--benchmark', action='store_true')
+    parser.add_argument('--train', action='store_true')
+    parser.add_argument('--timm', action='store_true')
     args = parser.parse_args()
 
     if args.native:
@@ -39,58 +41,87 @@ def main():
         torch.backends.cudnn.benchmark = False
 
     models_dict = torchvision.models.__dict__
-    models = ['resnet18',
-              'alexnet',
-              'vgg16',
-              'squeezenet1_0',
-              'densenet161',
-              #'inception_v3',
-              'googlenet',
-              'shufflenet_v2_x1_0', 
-              'mobilenet_v2',
-              'mobilenet_v3_large',
-              'mobilenet_v3_small',
-              'resnext50_32x4d',
-              'wide_resnet50_2',
-              'mnasnet1_0']
+    tv_models = ['resnet18',
+                'alexnet',
+                'vgg16',
+                'squeezenet1_0',
+                'densenet161',
+                #'inception_v3',
+                'googlenet',
+                'shufflenet_v2_x1_0', 
+                'mobilenet_v2',
+                'mobilenet_v3_large',
+                'mobilenet_v3_small',
+                'resnext50_32x4d',
+                'wide_resnet50_2',
+                'mnasnet1_0']
+    timm_models = ['efficientnet_b0',
+                   'mixnet_l',
+                   'seresnet152d',
+                   'seresnext26d_32x4d',
+                   'inception_v4']
+    if args.timm:
+        models = timm_models
+    else:
+        models = tv_models
     output = {'model': [], 'resolution': [], 'iter_time': []}
 
     assert args.sku in ['3080', '3090', 'A100', 'V100', 'A6000']
-    bar = Bar('models', max=len(models))
+    resolutions = generate_resolutions()
 
     for idx, model in enumerate(models):
-        batch_size = 64
-        if args.sku not in ['A100', 'A6000']:
-            batch_size = 32 
-        if args.sku in ['3080']:
+        if args.sku in ['A100', 'A6000']: # 48GiB
+            batch_size = 64
+        if args.sku in ['3080']: # 12GiB
             batch_size = 12
+        if args.sku in ['3090', 'A30']: # 24GiB
+            batch_size = 24
         if args.sku in ['V100']:
             batch_size = 48
 
-        resolutions = generate_resolutions()
-        m = models_dict[model]().cuda()
+        if not args.timm:
+            m = models_dict[model]().cuda()
+        else:
+            import timm
+            m = timm.create_model(model).cuda()
+        if args.train:
+            optimizer = torch.optim.SGD(m.parameters(), 3e-4,
+                                        momentum=0.9,
+                                        weight_decay=1e-4)
+            criterion = torch.nn.CrossEntropyLoss().cuda()
+
         if 'densenet' in model or 'wide_resnet' in model:
             batch_size //= 4
         elif 'resnext50_32x4d' in model:
             batch_size //= 4
-        for resolution in resolutions:
+        for res_idx, resolution in enumerate(resolutions):
             data = torch.rand(batch_size, 3, resolution[0], resolution[1], device='cuda')
+            _, target = torch.max(torch.rand(batch_size, 1000, device='cuda'), axis=1)
             for i in range(WARMUP_ITERS):
                 o = m(data)
+                if args.train:
+                    loss = criterion(o, target)
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
             torch.cuda.synchronize()
             t1 = time.time()
             for i in range(RUN_ITERS):
                 o = m(data)
+                if args.train:
+                    loss = criterion(o, target)
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
             torch.cuda.synchronize()
             t2 = time.time()
             iter_time = (t2-t1)/RUN_ITERS
             output['model'].append(model)
             output['resolution'].append(resolution)
             output['iter_time'].append(iter_time)
-        bar.next()
+            print(f"bench: {args.benchmark} train: {args.train} {model} ({idx}/{len(models)}) {resolution} ({res_idx}/{len(resolutions)}) {iter_time:.5f}", end='\r')
         if args.dry_run and idx >= 2:
             break
-    bar.finish()
     if args.output is not None:
         assert len(output['model']) == len(output['resolution'])
         assert len(output['iter_time']) == len(output['model'])
